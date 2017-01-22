57314ba68f283f22faa2ac80d1af5fe91070d68b
André Breda <addobandre99@gmail.com>
2017-01-22 22:17:42 +0000

    Add unit test skeleton for Backend Connection and Replicator
    Just describing what will be done.

---
+++ test/icarus/backend/backend.js
@@ -0,0 +1,27 @@
+import test from 'ava'
+
+test.todo('constructs')
+
+test.todo('connects to backend (WebSocket)')
+
+test.todo('handles socket event: connection')
+
+test.todo('handles socket event: reconnection')
+
+test.todo('handles socket event: reconnection error')
+
+test.todo('handles socket event: disconnection')
+
+test.todo('handle backend request: info')
+
+test.todo('handle backend request: replicate')
+
+test.todo('disconnects from backend')
+
+test.todo('backend disconnection: resets state')
+
+test.todo('backend disconnection: stops replicators')
+
+test.todo('cleanup: cleans up the replicator instances')
+
+test.todo('cleanup: cleans itself up')
---
+++ test/icarus/backend/replicator.js
@@ -0,0 +1,23 @@
+import test from 'ava'
+
+test.todo('replicates')
+
+test.todo('stops replication before cleanup')
+
+test.todo('stops replication when asked')
+
+test.todo('restarts replication after unexpected errors')
+
+test.todo('does not restart replication when inactive')
+
+test.todo('does not restart replication when cleaning up')
+
+test.todo('attempts last-minute one-shot replication during cleanup')
+
+test.todo('notifies state change: inactive')
+
+test.todo('notifies state change: stopped')
+
+test.todo('notifies state change: active')
+
+test.todo('notifies state change: cleanup')
