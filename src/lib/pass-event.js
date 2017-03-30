7c372c3366e4db6922715e09ba58258c76ad7a4e
André Breda <addobandre99@gmail.com>
2017-03-30 19:13:52 +0100

    pass-event: Make the target event name, the source event name by default
    Allows for the simplest use case without someone needing to repeat themselves:
    `passEvent(emitterFrom, emitterTo, 'the event to forward')`

--- src/lib/pass-event.js
+++ src/lib/pass-event.js
@@ -4,6 +4,6 @@
  * @param {String} sourceEvent
  * @param {String} destEvent
  */
-export default function passEvent(sourceEmitter, destEmitter, sourceEvent, destEvent) {
+export default function passEvent(sourceEmitter, destEmitter, sourceEvent, destEvent = sourceEvent) {
 	sourceEmitter.on(sourceEvent, (...args) => destEmitter.emit(destEvent, ...args))
 }
