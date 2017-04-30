ac06773cba8891c69785656d024e66a6decd6514
André Breda <addobandre99@gmail.com>
2017-04-30 22:52:48 +0100

    perf(backend): Stop replicators when stopping the backend connection
    Replicators use memory. Memory may be precious. If the station is slowing down
    too much, the operator may choose to disable the replication process.

--- src/icarus/backend/index.js
+++ src/icarus/backend/index.js
@@ -123,8 +123,10 @@ export default class Backend extends EventEmitter {
 		// Finally remove the socket reference here
 		this._socket = null
 
-		// No need to stop replicators, they're harmless
-		// if you need them gone, disconnect from the internet
+		// Stop the replicators
+		// The operator may be experiencing issues and prefer to keep them off
+		this.dataReplicator.stop()
+		this.logReplicator.stop()
 
 		this._updateState('inactive')
 	}
